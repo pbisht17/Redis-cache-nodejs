ee1cb98db4bba5d755a6d4989e6cf32de71a510b
unknown <pawanbisht83@gmail.com>
2024-01-21 16:34:06 +0530

    new changes

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,11 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function(){
     console.log('Inside of the mongoose query');
+    console.log(this.getQuery()); //
+    console.log(this.mongooseCollection.name); //returns the name of collection
+    console.log(this.getOptions());
+    Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    })
     return exec.apply(this, arguments);
 }
